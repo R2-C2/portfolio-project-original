a3e49ce2a7ec198c1e20f992709706036b33da8f
R2-C2 <royce.crenshaw@gmail.com>
2020-10-19 12:42:24 -0500

    Made blog section easier to navigate; added functionality and readability

--- blog/models.py
+++ blog/models.py
@@ -5,3 +5,12 @@ class Blog(models.Model):
 	title = models.CharField(max_length=100)
 	body = models.TextField()
 	pub_date = models.DateTimeField()
+
+	def summary(self):
+		return self.body[:100]
+
+	def pub_date_neat(self):
+		return self.pub_date.strftime('%b %e %Y')
+
+	def __str__(self):
+		return self.title
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Blog
 
-# Create your views here.
+def all_blogs(request):
+    blogs = Blog.objects
+    return render(request, 'all_blogs.html', {"blogs":blogs})
+
+def detail(request, blog_id):
+    blog_detail = get_object_or_404(Blog, pk=blog_id)
+    return render(request, 'blog_detail.html', {'blog':blog_detail})
