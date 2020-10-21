97dda6131496bb27cb80ca817a67ebc41800f174
R2-C2 <royce.crenshaw@gmail.com>
2020-10-21 14:40:06 -0500

    settings update

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 STATICFILES_DIRS = [
     BASE_DIR / 'portfolio/static/'
 ]
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = BASE_DIR / 'portfolio/static'
 STATIC_URL = '/static/'
 
 MEDIA_ROOT = BASE_DIR / 'media'
