ce4ceef479d7a041efe04aa9d587dc1c898b53cc
R2-C2 <royce.crenshaw@gmail.com>
2020-11-12 17:07:33 -0600

    Updated blog body; updated job icons

--- jobs/models.py
+++ jobs/models.py
@@ -2,14 +2,31 @@ from django.db import models
 
 class Job(models.Model):
 	image = models.ImageField(upload_to='images/')
-	summary = models.CharField(max_length=200)
+	company = models.CharField(max_length=200, blank=True)
+	title = models.CharField(max_length=200, blank=True)
+	summary = models.TextField(max_length=2000)
+	start_date = models.DateField()
+	end_date = models.DateField()
 
 	def __str__(self):
-		return self.summary
+		return self.title
+
+	def start_date_neat(self):
+		return self.start_date.strftime('%m/%Y')
+
+	def end_date_neat(self):
+		return self.end_date.strftime('%m/%Y')
 
 class Certificate(models.Model):
 	image = models.ImageField(upload_to='images/')
-	summary = models.CharField(max_length=200)
+	issuer = models.CharField(max_length=200, blank=True)
+	topic = models.CharField(max_length=200, blank=True)
 
 	def __str__(self):
-		return self.summary
+		return self.topic
+
+
+'''
+python manange.py makemigrations
+python manange.py migrate
+'''
